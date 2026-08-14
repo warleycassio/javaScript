f69159a4955fdbf2cb8abfe504f59f5462a43c2c
warley cassio <100567459+warleycassio@users.noreply.github.com>
2026-08-14 14:31:32 -0300

    ex009

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`A velocidade  do seu Carro é ${vel}Kmm/h`)
+if (vel > 60) {
+            console.log(`Você ultrapassou a  velocidade permmitida, MULTADO!`)
+}
+
+console.log(`Dirija Sempre usando sinto  de segurança!`)
