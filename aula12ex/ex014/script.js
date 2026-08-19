fb2663324d6909ecba85250539b9c12cb48146b0
warley cassio <100567459+warleycassio@users.noreply.github.com>
2026-08-19 16:52:54 -0300

    ex15

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,16 +2,20 @@ function carregar() {
     var msg = window.document.getElementById(`msg`)
     var img =window.document.getElementById(`foto`)
     var data = new Date()
-   // var hora = data.getHours()
-   var hora = 15
+   var hora = data.getHours()
+   //var hora = 7
 
     msg.innerHTML = `Agora são ${hora} horas. `
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.png'
+      
+        document.body.style.background = '#e2cd9f'
 
     } else if (hora >= 12 && hora < 18) {
         img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
     } else {
         img.src = 'noite.png'
+        document.body.style.background = '#515154'
     }
 }
