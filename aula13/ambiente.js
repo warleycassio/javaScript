b3597b3980cdcedc2ac9c5b320023a80125ce014
warley cassio <100567459+warleycassio@users.noreply.github.com>
2026-08-25 14:02:38 -0300

    certo

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+var c= 1
+while (c <=10) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+
+
+
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
---
+++ aula13/node_300c8c2c50100.tmp.js
@@ -0,0 +1,20 @@
+var c= 1
+while (c <=6) {
+    console.log('Tudo bem?')
+    c
+}
+
+
+
+
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
