0ecebbc9f2f04af730b44e9b4677cd78fdfa6628
warley cassio <100567459+warleycassio@users.noreply.github.com>
2026-08-25 14:09:58 -0300

    whie e do

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,17 +1,15 @@
 var c= 1
-while (c <=10) {
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <=6)
+
+/*var c= 1
+while (c <=6) {
     console.log(`passo ${c}`)
     c++
 }
-
-
-
-
-
-
-
-
-
+*/ 
 /*console.log('tudo bem?')
 console.log('tudo bem?')
 console.log('tudo bem?')
